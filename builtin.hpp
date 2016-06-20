c95b82939d81eb1d6f3cce8fa3d3f7be4915a984
zhouwanchun1993 <zhouwanchun1993@163.com>
2016-06-20 14:23:49 +0800

    add reduce iterfunc

--- builtin.hpp
+++ builtin.hpp
@@ -23,3 +23,13 @@ std::list<T> l(T value, Args... args)
     }
     return retValue;
 }
+
+#define _reduce(Func,IterBegin,IterEnd,Result) \
+    {auto __it = IterBegin; \
+    if(__it != IterEnd){ \
+    Result = *__it++; \
+    for(;__it!=IterEnd;++__it) \
+    {Result = Func(Result,*__it);}}}
+
+#define reduce(Func,Squence,Result) \
+    _reduce(Func,Squence.begin(),Squence.end(),Result)
