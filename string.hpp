346ef2f2f9a60fe7d94c10a97a751809ae85e477
zhouwanchun1993 <zhouwanchun1993@163.com>
2016-07-03 19:55:12 +0800

    decode string to unicode and encode string as other character set(only for windows platform)

--- string.hpp
+++ string.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <string>
+#include <memory>
+#ifdef _WIN32
+    #include <Windows.h>
+#endif
 
 namespace string
 {
@@ -48,5 +52,32 @@ namespace string
             }
         }
         return  result;
+    }
+
+    std::wstring decode(UINT inputcs, std::string str)
+    {
+        std::wstring out;
+        if (str.length() != 0)
+        {
+            int dwUnicodeLen = MultiByteToWideChar(inputcs, 0, str.c_str(), -1, NULL, 0);
+            size_t num = dwUnicodeLen*sizeof(wchar_t);
+            std::auto_ptr<wchar_t> pwText(new wchar_t[num]());   // init wchar_t array with () syntax
+            MultiByteToWideChar(inputcs, 0, str.c_str(), -1, pwText.get(), dwUnicodeLen);
+            out = pwText.get();
+        }
+        return out;
+    }
+
+    std::string encode(UINT inputcs, std::wstring wstr)
+    {
+        std::string out;
+        if (wstr.length() != 0)
+        {
+            int dwMultiByteLen = WideCharToMultiByte(inputcs, 0, wstr.c_str(), -1, NULL, 0, NULL, NULL);
+            std::auto_ptr<char> pwText(new char[dwMultiByteLen]());   // init char array with () syntax
+            WideCharToMultiByte(inputcs, 0, wstr.c_str(), -1, pwText.get(), dwMultiByteLen,NULL,NULL);
+            out = pwText.get();
+        }
+        return out;
     }
 }
